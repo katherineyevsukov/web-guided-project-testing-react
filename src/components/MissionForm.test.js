f94060c8f4d4f5351fbcd12daf57a63bfa48e189
Warren C. Longmire <warren.longmire@lambdaschool.com>
2021-09-13 12:37:54 -0400

    lecture work-in-progress

--- src/components/MissionForm.test.js
+++ src/components/MissionForm.test.js
@@ -6,3 +6,15 @@ test("renders without an error", ()=> {
     render(<MissionForm/>);
 });
 
+test("renders loading message when isFetchingData === true", ()=> {
+    
+});
+
+test("renders button when isFetchingData === false", ()=> {
+
+});
+
+test("execute getData when button is clicked", ()=> {
+
+});
+
