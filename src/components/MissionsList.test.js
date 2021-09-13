87da3d026356d6648256aa6f71f0f47132599df3
Warren C. Longmire <warren.longmire@lambdaschool.com>
2021-09-13 13:55:57 -0400

    lecture work-in-progress

--- src/components/MissionsList.test.js
+++ src/components/MissionsList.test.js
@@ -6,6 +6,18 @@ test("renders without errors", () => {
     render(<MissionsList missions={[]}/>);
 });
 
+
+const testMissions = [
+    {
+        mission_name: "mission 1",
+        mission_id: 1
+    },
+    {
+        mission_name: "mission 2",
+        mission_id: 2
+    }
+]
+
 test("renders new tests when passed into component", () => {
     //Arrange 1: renders MissionsList with no missions
     const { rerender } = render(<MissionsList missions={[]}/>)
@@ -15,9 +27,8 @@ test("renders new tests when passed into component", () => {
     //Assert 1: checks that no missions are on the screen
     expect(missions).toHaveLength(0);
 
-    console.log(renderStuff);
-
     //Arrange 2: renders MissionsList with 3 missions
+    rerender(<MissionsList missions={testMissions}/>);
     //Act 2: gets the missions from our component
     //Assert 2: checks that 3 missions are on the screen.
 })
