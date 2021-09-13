5f6ceb28dbef35a072fbd878c7a5d07f3f8601ea
Warren C. Longmire <warren.longmire@lambdaschool.com>
2021-09-13 13:52:54 -0400

    lecture work-in-progress

--- src/components/MissionsList.test.js
+++ src/components/MissionsList.test.js
@@ -10,9 +10,10 @@ test("renders new tests when passed into component", () => {
     //Arrange 1: renders MissionsList with no missions
     render(<MissionsList missions={[]}/>)
     //Act 1: gets the missions from our component
-    const missions = screen.getByAllTestId('mission');
-    
+    const missions = screen.queryAllByAltText('mission');
+
     //Assert 1: checks that no missions are on the screen
+    expect(missions).toHaveLength(0);
 
     //Arrange 2: renders MissionsList with 3 missions
     //Act 2: gets the missions from our component
